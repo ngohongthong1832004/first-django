99462602903ef8b28208c7b651f38e2b17a59586
ngohongthong <baphongpine@gmail.com>
2023-05-09 20:49:21 +0700

    :)) ngu moi tao branch day

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=100)
-    password = forms.CharField(label="Password", max_length=100)
+    password = forms.CharField(label="Password", max_length=100,
+        widget=forms.PasswordInput
+    )
 
 
 
